f1a21473f5520863230ccb759578db50bbd0965c
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-08-31 23:35:42 +0530

    fix: use BrowserRouter with declarative route definitions

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import './styles/globals.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import mainRoutes from './routes/AppRoutes'
 
 function App() {
-  const router = createBrowserRouter(mainRoutes)
-
-  return <RouterProvider router={router} />
+  return <BrowserRouter>{mainRoutes()}</BrowserRouter>
 }
 
 export default App
