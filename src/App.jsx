1e59b4345eaaba3fbbfdd6e68421e618aa9df67e
Dev Prakash <141032391+devprakash11@users.noreply.github.com>
2026-09-01 10:01:50 +0530

    fix: normalize app stylesheet imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './styles/global.css'
-import './styles/allpage.css'
+import './styles/allPage.css'
 import { BrowserRouter } from 'react-router-dom'
 import mainRoutes from './routes/AppRoutes'
 
 function App() {
-  return <BrowserRouter>{mainRoutes()}</BrowserRouter>
+  return (
+    <BrowserRouter>
+      {mainRoutes()}
+    </BrowserRouter>
+  )
 }
 
 export default App
